31eb07074aee2751396f511364a8953acabee53b
Boxing <boxingp@gmail.com>
2023-06-05 17:38:22 +0800

    VR-4: send email with excel file attached to server contact.

--- convert_qualys_excel.py
+++ convert_qualys_excel.py
@@ -3,6 +3,45 @@ import re
 import sys
 
 import pandas as pd
+import psycopg2
+
+from emails.emails import Emails
+
+
+def format_emails(contacts):
+    values = re.split(r'[;\n]', contacts)
+    emails = [value.strip() for value in values if re.match(r'[^@\s]+@[^@\s]+\.[^@\s]+', value)]
+    return emails
+
+
+def get_it_contact(server_list, server_name):
+    for server in server_list:
+        if server['server_name'].upper() == server_name.upper():
+            return format_emails(server['it_contact'])
+    return []
+
+
+def get_server_contact():
+    connection = psycopg2.connect(
+        host="",
+        database="",
+        port="",
+        user="",
+        password=""
+    )
+    query = "SELECT server_name, it_contact FROM on_premise_server"
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+    results = []
+    for row in rows:
+        result = {
+            'server_name': row[0],
+            'it_contact': row[1]
+        }
+        results.append(result)
+    connection.close()
+    return results
 
 
 def format_name(string):
@@ -31,8 +70,25 @@ for name in sheet_names:
 dfs = pd.read_excel(data_file, sheet_name=[data_sheet])
 df = dfs[data_sheet]
 
+server_contact = get_server_contact()
+
 groups = df.groupby('Asset Name')
 for name, group in groups:
-    file_name = f'{format_name(name)}.xlsx'
+    server_name = format_name(name)
+    file_name = f'{server_name}.xlsx'
+    it_contact = get_it_contact(server_contact, server_name)
     file_path = os.path.join(output_directory, file_name)
     group.to_excel(file_path, index=False)
+    severity_counts = group['Severity'].value_counts().to_dict()
+
+    summary = {
+        "server_name": server_name,
+        "contact_email": it_contact,
+        "report_path": file_path,
+        "vulnerabilities_severity": [
+            {"level": severity, "total": count}
+            for severity, count in severity_counts.items()
+        ]
+    }
+    if it_contact:
+        Emails().send_email(summary)
---
+++ emails/emails.py
@@ -0,0 +1,59 @@
+import os
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class Emails(object):
+    def __init__(self):
+        self.smtp_server = ''
+        self.port =
+        self.sender_email = ''
+        self.subject = ''
+        with open(os.path.join(os.path.dirname(__file__), 'logo.png'), 'rb') as file:
+            self.logo_img = file.read()
+        with open(os.path.join(os.path.dirname(__file__), 'email_template.html'), 'r', encoding='UTF-8') as file:
+            self.html = file.read()
+
+    def extract_names(self, email_list):
+        names = [email.split('@')[0] for email in email_list]
+        names = [name.split('.')[0].capitalize() for name in names]
+        if len(names) == 1:
+            return names[0]
+        elif len(names) == 2:
+            return names[0] + ' and ' + names[1]
+        else:
+            return ', '.join(names[:-1]) + ', and ' + names[-1]
+
+    def send_email(self, data):
+        message = MIMEMultipart("alternative")
+        message["Subject"] = self.subject
+        message["From"] = self.sender_email
+        message["To"] = ",".join(data['contact_email'])
+        html_part = MIMEMultipart("related")
+        first_names = self.extract_names(data['contact_email'])
+        self.html = self.html.replace('${FIRST_NAME}', first_names)
+        severity_totals = {
+            item['level']: item['total']
+            for item in data['vulnerabilities_severity']
+        }
+        self.html = self.html.replace('${CRITICAL_NUMBER}', str(severity_totals.get(5, 0)))
+        self.html = self.html.replace('${HIGH_NUMBER}', str(severity_totals.get(4, 0)))
+        html_part.attach(MIMEText(self.html, "html"))
+        logo_image = MIMEImage(self.logo_img)
+        logo_image.add_header('Content-ID', '<logo>')
+        html_part.attach(logo_image)
+        message.attach(html_part)
+
+        with open(data['report_path'], 'rb') as attachment:
+            attachment_obj = MIMEBase('application', 'octet-stream')
+            attachment_obj.set_payload(attachment.read())
+        encoders.encode_base64(attachment_obj)
+        attachment_obj.add_header('Content-Disposition', f"attachment; filename= {data['server_name']}.xlsx")
+        message.attach(attachment_obj)
+
+        with smtplib.SMTP(self.smtp_server, self.port) as server:
+            server.sendmail(from_addr=self.sender_email, to_addrs=data['contact_email'], msg=message.as_string())
